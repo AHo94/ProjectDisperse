fba8bd499810c5e456eb16b8c5983eb48aff31d0
Alex <hoalex94@gmail.com>
2017-10-04 14:07:02 +0200

    Fixed folder

--- Disperse_analysis.py
+++ Disperse_analysis.py
@@ -1819,7 +1819,7 @@ if __name__ == '__main__':
 					p = mp.Pool(1)
 					sigma_values = np.linspace(3, 10, 4)
 					Instance = FilamentsPerSigma.FilamentsPerSigma(file_directory+'/'+LCDM_z0_64_dir+'SkelconvOutput_LCDMz064.a.NDskl')
-					results_dir = os.path.join(savefile_directory, LCDM_z0_64_dir+'Plotstest2')
+					results_dir = os.path.join(savefile_directory, LCDM_z0_64_dir+'Plotstest2/')
 					if not os.path.isdir(results_dir):
 						os.makedirs(results_dir)
 
