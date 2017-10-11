3e466acbae35b4aa2132487cb8923d1a9447d00f
Alex <hoalex94@gmail.com>
2017-10-11 15:49:36 +0200

    Added histogram comparison class to its own module

---
+++ Histogram_comparison.py
@@ -0,0 +1,287 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+class Histogram_Comparison():
+	def __init__(self, savefile, savefigDirectory, savefile_directory, filetype, redshift, LCDM=False, SymmA=False, SymmB=False, nsigComparison=False):
+		self.savefile = savefile
+		self.LCDM_check = LCDM
+		self.SymmA_check = SymmA
+		self.SymmB_check = SymmB
+		self.filetype = filetype
+
+		self.ParticleComparison = False
+		self.ModelComparison = False
+		self.SigmaComparison = False
+
+		self.results_dir = os.path.join(savefile_directory, savefigDirectory)
+		if not os.path.isdir(self.results_dir):
+			os.makedirs(self.results_dir)
+
+		if LCDM and not SymmA and not SymmB:
+			self.ModelFilename = 'LCDM' + 'z' + str(redshift)
+			self.ParticleComparison = True
+		elif not LCDM and SymmA and not SymmB:
+			self.ModelFilename = 'SymmA' + 'z' + str(redshift)
+			self.ParticleComparison = True
+		elif not LCDM and not SymmA and SymmB:
+			self.ModelFilename = 'SymmB' + 'z' + str(redshift)
+			self.ParticleComparison = True
+		elif LCDM and SymmA and not SymmB == 0:
+			self.ModelFilename = 'LCDM_SymmB' + 'z' + str(redshift)
+			self.ModelComparison = True
+		elif LCDM and not SymmA and SymmB:
+			self.ModelFilename = 'LCDM_SymmB' + 'z' + str(redshift)
+			self.ModelComparison = True
+		elif LCDM and SymmA and not SymmB:
+			self.ModelFilename = 'LCDM_SymmB' + 'z' + str(redshift)
+			self.ModelComparison = True
+		elif LCDM and SymmA and SymmB:
+			self.ModelFilename = 'LCDM_SymmA_SymmB' + 'z' + str(redshift)
+		else:
+			raise ValueError('At least one model must be set to compare!')
+
+		if nsigComparison:
+			self.ModelComparison = 0
+			self.ParticleComparison = 0
+			self.SigmaComparison = 1
+
+			self.ModelFilename += 'SigmaCompare'
+
+	def Run(self, NumberConnections, FilamentLengths, NPointsPerFilament, nPart=False):
+		if not nPart:
+			self.ModelFilename += self.filetype
+		elif nPart == 64:
+			self.ModelFilename += '64Part' + self.filetype
+		elif nPart == 128:
+			self.ModelFilename += '128Part' + self.filetype
+		elif nPart == 256:
+			self.ModelFilename += '256Part' + self.filetype
+		elif nPart == 512:
+			self.ModelFilename += '512Part' + self.filetype
+		self.nParticles = nPart
+
+		if type(NumberConnections) != list:
+			raise ValueError('Argument NumberConnections must be a list!')
+		elif type(FilamentLengths) != list:
+			raise ValueError('Argument FilamentLengths must be a list!')
+		
+		if len(NumberConnections) < 2:
+			raise ValueError('Nothing to compare because NumberConnections has less than two arrays!')
+		elif len(FilamentLengths) < 2:
+			raise ValueError('Nothing to compare because FilamentLengths has less than two arrays!')
+
+		if len(NumberConnections) != len(FilamentLengths):
+			raise ValueError('List size of NumberConnections and FilamentLengths must be equal!')
+
+		if not self.SigmaComparison:
+			for i in range(len(NumberConnections)-1):
+				if len(NumberConnections[i+1]) < len(NumberConnections[i]):
+					raise ValueError('List of NumberConnections must be in order to increasing number of particles')
+				elif len(FilamentLengths[i+1]) < len(FilamentLengths[i]):
+					raise ValueError('List of FilamentLengths must be in order to increasing number of particles')
+				elif len(NPointsPerFilament[i+1]) < len(NPointsPerFilament[i]):
+					raise ValueError('List of Number of points per filament must be in order to increasing number of particles')
+				
+
+		self.NumberConnections = NumberConnections
+		self.FilamentLengths = FilamentLengths
+		self.NPointsPerFilament = NPointsPerFilament
+
+		self.N = len(self.NumberConnections)
+		self.Check_Number_Comparisons()
+		self.Plot_Histograms_particleComparison()
+
+
+	def Check_Number_Comparisons(self):
+		if self.ParticleComparison:
+			if self.N == 2:
+				self.LegendText = ['$\mathregular{64^3}$ particles', '$\mathregular{128^3}$ particles']
+			elif self.N == 3:
+				self.LegendText = ['$\mathregular{64^3}$ particles', '$\mathregular{128^3}$ particles', '$\mathregular{256^3}$ particles']
+			elif self.N == 4:
+				self.LegendText = ['$\mathregular{64^3}$ particles', '$\mathregular{128^3}$ particles', \
+									'$\mathregular{256^3}$ particles', '$\mathregular{512^3}$ particles']
+		elif self.ModelComparison:
+			self.LegendText = []
+			if self.LCDM_check:
+				self.LegendText.append('LCDM')
+			if self.SymmA_check:
+				self.LegendText.append('Symm_A')
+			if self.SymmB_check:
+				self.LegendText.append('Symm_B')
+		elif self.SigmaComparison:
+			self.LegendText = ['$\mathregular{\sigma=3}$', '$\mathregular{\sigma=4}$', '$\mathregular{\sigma=5}$']
+
+
+	def Plot_Histograms_particleComparison(self):
+		alphas = [0.4, 0.5, 0.6, 0.7]
+		#alphas = [0.6, 0.5, 0.4, 0.3]
+		if self.nParticles == 512:
+			subsample_text = 's'
+		else:
+			subsample_text = ' subsample'
+
+		ConnectedHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(self.N):
+			DataMin = min(self.NumberConnections[i])
+			DataMax = max(self.NumberConnections[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(self.NumberConnections[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of connected filaments')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number filament connections \n with '+str(self.nParticles) + '$\mathregular{^3}$ particle'+subsample_text)		
+		plt.legend(self.LegendText)
+		plt.hold(False)
+	
+		LengthHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(self.N):
+			plt.hist(self.FilamentLengths[i], align='mid', rwidth=1, bins=400, normed=False, histtype='step')
+		plt.xlabel('Filament lengths')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of filament length \n with ' +str(self.nParticles) + '$\mathregular{^3}$ particle'+subsample_text)
+		plt.legend(self.LegendText)
+		plt.hold(False)
+
+		NPointsHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(self.N):
+			DataMin = min(self.NPointsPerFilament[i])
+			DataMax = max(self.NPointsPerFilament[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(self.NPointsPerFilament[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of points per filament')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number of datapoints per filament \n with ' +str(self.nParticles) + '$\mathregular{^3}$ particle'+subsample_text)
+		plt.legend(self.LegendText)
+		plt.hold(False)
+
+		if self.savefile == 1:
+			print '--- SAVING IN: ', self.results_dir, ' ---'
+			ConnectedHistComparison.savefig(self.results_dir + 'HistNumConnectedFilamentsComparison' + self.ModelFilename)
+			LengthHistComparison.savefig(self.results_dir + 'HistLengthComparison' + self.ModelFilename)
+			NPointsHistComparison.savefig(self.results_dir + 'HistNPointsComparison' + self.ModelFilename)
+		elif self.savefile == 2:
+			print 'Done! No histograms to plot.'
+		else:
+			plt.show()
+		plt.close('all')
+
+	def Convergence_tests(self, Nconnections, FilLengths, NptsPerFilament):
+		if len(Nconnections) != len(FilLengths) or len(Nconnections) != len(NptsPerFilament) or len(FilLengths) != len(NptsPerFilament):
+			raise ValueError('Lists containing the histograms are not of equal length!')
+
+		alphas = [0.65, 0.6, 0.55, 0.5, 0.45, 0.4]
+		Legends = ['$\mathregular{\sigma=5}, 64^3$ part subsample', '$\mathregular{\sigma=4},\
+					 64^3$ part subsample', '$\mathregular{\sigma=3}, 64^3$ part subsample', \
+					'$\mathregular{\sigma=5}, 512^3$ part', '$\mathregular{\sigma=4}, 512^3$ part', '$\mathregular{\sigma=3}, 512^3$ part']
+		N = len(Nconnections)
+		ConnectedHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			DataMin = min(Nconnections[i])
+			DataMax = max(Nconnections[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(Nconnections[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of connected filaments')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number connections per filament')
+		plt.legend(Legends)
+		plt.hold(False)
+
+		LengthHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			plt.hist(FilLengths[i], align='mid', rwidth=1, bins=400, normed=False, histtype='step')
+		plt.xlabel('Filament lengths')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of filament lengths')
+		plt.legend(Legends)
+		plt.hold(False)
+
+		NPointsHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			DataMin = min(NptsPerFilament[i])
+			DataMax = max(NptsPerFilament[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(NptsPerFilament[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of points per filament')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number data points per filament')
+		plt.legend(Legends)
+		plt.hold(False)
+
+		if self.savefile == 1:
+			print '--- SAVING IN: ', self.results_dir, ' ---'
+			ConnectedHistComparison.savefig(self.results_dir + 'HistNumConnectedFilamentsComparison_64and512Part' + self.ModelFilename)
+			LengthHistComparison.savefig(self.results_dir + 'HistLengthComparison_64and512Part' + self.ModelFilename)
+			NPointsHistComparison.savefig(self.results_dir + 'HistNPointsComparison_64and512Part' + self.ModelFilename)
+		elif self.savefile == 2:
+			print 'Done! No histograms to plot.'
+		else:
+			plt.show()
+		plt.close('all')
+
+	def Sigma_plot_comparison(self, Nconnections, FilLengths, NptsPerFilament, nsigma):
+		if len(Nconnections) != len(FilLengths) or len(Nconnections) != len(NptsPerFilament) or len(FilLengths) != len(NptsPerFilament):
+			raise ValueError('Lists containing the histograms are not of equal length!')
+
+		Legends = ['$64^3$ part subsample', '$128^3$ part subsample', '$256^3$ part subsample', '$512^3$ particles']
+		alphas = [0.7, 0.6, 0.5, 0.4]
+		N = len(Nconnections)
+		ConnectedHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			DataMin = min(Nconnections[i])
+			DataMax = max(Nconnections[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(Nconnections[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of connected filaments')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number connections per filament for $\mathregular{\sigma=}$' + str(nsigma))
+		plt.legend(Legends)
+		plt.hold(False)
+
+		LengthHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			plt.hist(FilLengths[i], align='mid', rwidth=1, bins=400, normed=False, histtype='step')
+		plt.xlabel('Filament lengths')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of filament lengths for $\mathregular{\sigma=}$' + str(nsigma))
+		plt.legend(Legends)
+		plt.hold(False)
+
+		NPointsHistComparison = plt.figure()
+		plt.hold(True)
+		for i in range(N):
+			DataMin = min(NptsPerFilament[i])
+			DataMax = max(NptsPerFilament[i])
+			BinSize = (DataMax - DataMin)/(0.5) + 1
+			BinList = np.linspace(DataMin, DataMax, BinSize)
+			plt.hist(NptsPerFilament[i], align='mid', rwidth=1, bins=BinList, normed=False, alpha=alphas[i], histtype='step')
+		plt.xlabel('Number of points per filament')
+		plt.ylabel('Number of occurances')
+		plt.title('Histogram comparison of number data points per filament for $\mathregular{\sigma=}$' + str(nsigma))
+		plt.legend(Legends)
+		plt.hold(False)
+
+		if self.savefile == 1:
+			print '--- SAVING IN: ', self.results_dir, ' ---'
+			ConnectedHistComparison.savefig(self.results_dir + 'HistNumConnectedFilamentsComparison_AllParticles_nsig' + str(nsigma) + self.filetype)
+			LengthHistComparison.savefig(self.results_dir + 'HistLengthComparison_AllParticles_nsig' + str(nsigma) + self.filetype)
+			NPointsHistComparison.savefig(self.results_dir + 'HistNPointsComparison_AllParticles_nsig' + str(nsigma) + self.filetype)
+		elif self.savefile == 2:
+			print 'Done! No histograms to plot.'
+		else:
+			plt.show()
+		plt.close('all')
+
