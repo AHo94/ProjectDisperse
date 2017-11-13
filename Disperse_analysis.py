92ea654201f8bc3f0c08a0ebdefd149a9863afd4
Alex Ho <hoalex94@gmail.com>
2017-11-13 12:29:20 +0100

    Small typo fix. Pickle file saving function should work fine now.

--- Disperse_analysis.py
+++ Disperse_analysis.py
@@ -1087,7 +1087,7 @@ def Save_NumPartPerFil(name, FilPos):
 		raise ValueError('Model input name %s not correctly set into the gadget file reader.' %modelfile)
 
 	cachedir_ppf = '/mn/stornext/d13/euclid/aleh/PythonCaches/Disperse_analysis/ParticlesPerFilament/'
-	if not os.path.isdir(cahcedir_ppf):
+	if not os.path.isdir(cachedir_ppf):
 		os.makedirs(cachedir_ppf)
 		
 	cachedir_lcdm = cachedir_ppf + name + '_256Part.p'
